e00b7055ea8855dd2fab7447bc7fa9fad54c5413
youseokhwan <youseokhwan15@gmail.com>
2022-09-24 01:18:45 +0900

    dummy unit test 추가

---
+++ GitHubActionTest/GitHubActionTestTests/GitHubActionTestTests.swift
@@ -0,0 +1,22 @@
+//
+//  GitHubActionTestTests.swift
+//  GitHubActionTestTests
+//
+//  Created by 유석환 on 2022/09/24.
+//
+
+import XCTest
+
+final class GitHubActionTestTests: XCTestCase {
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
